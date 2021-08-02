d16e5ac97d6a872a0798d4644b23926c2126d9eb
swathi0695 <37923510+swathi0695@users.noreply.github.com>
2021-08-02 10:57:16 +0530

    Update single_vm_status.py

--- actions/single_vm_status.py
+++ actions/single_vm_status.py
@@ -14,28 +14,26 @@ from msrestazure.azure_exceptions import CloudError
 
 from haikunator import Haikunator
 
-
 class SingleVmStatus(AzureBaseAction):
 
     def run(self, vm):
 
         credentials, subscription_id = self.get_credentials()
         compute_client = ComputeManagementClient(credentials, subscription_id)
-
+        result = {}
         try:
-		for v in compute_client.virtual_machines.list_all():	
-			if v.name == vm:
-				vm_details = v.id.split("/")
-				res_grp = vm_details[4]
-				vm_name = vm_details[-1]
-				statuses = compute_client.virtual_machines.instance_view(
-						res_grp, vm_name).statuses
-				status = len(statuses) >= 2 and statuses[1]
-				state = status.code.split('/')[-1]
-				print("VM Name : {} \n \tState : {} ".format(v.name, state.capitalize()))
-				result = {"status" : state}
+            for v in compute_client.virtual_machines.list_all():
+                if v.name == vm:
+                    vm_details = v.id.split("/")
+                    res_grp = vm_details[4]
+                    vm_name = vm_details[-1]
+                    statuses = compute_client.virtual_machines.instance_view(res_grp, vm_name).statuses
+                    status = len(statuses) >= 2 and statuses[1]
+                    state = status.code.split('/')[-1]
+                    print("VM Name : {} \n \tState : {} ".format(v.name, state.capitalize()))
+                    result = {"status": state}
         except CloudError:
             result = {"error": "A VM operation failed:\n" + traceback.format_exc()}
-			#raise result
+            # raise result
         return result
 
